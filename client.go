e6aab9eaafe20ce35a7afdd8b6d8d2e8058c43f1
j-weigle <justin.weigle2@gmail.com>
2021-08-17 02:52:47 -0400

    synchronization helper types and methods moved to their own file

--- client.go
+++ client.go
@@ -6,7 +6,6 @@ import (
 	"net/url"
 	"strings"
 	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -57,65 +56,6 @@ type client struct {
 	rcvdMsg          chan struct{}                 // when conn reads, notifies ping loop.
 	rcvdPong         chan struct{}                 // when pong received, notifies ping loop.
 	reconnectCounter int                           // for keeping track of reconnect attempts before a successful attempt.
-}
-
-type atomicBool struct{ val int32 }
-
-func (atom *atomicBool) set(val bool) {
-	if val {
-		atomic.StoreInt32(&atom.val, 1)
-	} else {
-		atomic.StoreInt32(&atom.val, 0)
-	}
-}
-func (atom *atomicBool) get() bool {
-	return atomic.LoadInt32(&atom.val) == 1
-}
-
-type connCloseErr struct {
-	mutex sync.Mutex
-	err   error
-}
-
-func (t *connCloseErr) update(err error) {
-	var override = err == ErrDisconnectCalled || err == ErrLoginFailure
-	t.mutex.Lock()
-	if t.err == nil {
-		t.err = err
-	} else if override {
-		if t.err != ErrDisconnectCalled && t.err != ErrLoginFailure {
-			t.err = err
-		}
-	}
-	t.mutex.Unlock()
-}
-
-// notifier's reset() and notify() methods are used in combination to notify multiple goroutines to close.
-// call reset() before spawning goroutines
-// call notify() in any goroutines to signal one another by listening to the notifier's channel ch
-type notifier struct {
-	mutex sync.Mutex
-	once  *sync.Once
-	ch    chan struct{}
-}
-
-// notify uses the notifier and makes it unusable until reset.
-func (n *notifier) notify() {
-	n.mutex.Lock()
-	n.once.Do(func() {
-		if n.ch != nil {
-			close(n.ch)
-		}
-	})
-	n.mutex.Unlock()
-}
-
-// reset sets the notifier to be ready to be used.
-func (n *notifier) reset() {
-	n.mutex.Lock()
-	n.once = &sync.Once{}
-	n.ch = make(chan struct{})
-	n.mutex.Unlock()
 }
 
 // NewClient returns a new client using the provided config.

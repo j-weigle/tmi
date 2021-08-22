17f6683482a371d90dbb7777128ddb8526487039
j-weigle <justin.weigle2@gmail.com>
2021-08-22 01:35:52 -0400

    test pingpong

--- irchandlers_test.go
+++ irchandlers_test.go
@@ -2,6 +2,7 @@ package tmi
 
 import (
 	"testing"
+	"time"
 )
 
 // NOTICE message handler
@@ -18,4 +19,44 @@ func TestLoginFailure(t *testing.T) {
 	if err != ErrLoginFailure {
 		t.Errorf("client was supposed to error on login authentication")
 	}
+}
+
+func TestPingPong(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skip:TestPingPong, reason:short mode")
+	}
+
+	config := NewClientConfig()
+	config.Identity.Anonymous()
+	config.Pinger.Disable()
+	c := NewClient(config)
+
+	rcvdPong := make(chan struct{})
+	c.OnPongMessage(func(m PongMessage) {
+		rcvdPong <- struct{}{}
+	})
+
+	go c.Connect()
+	time.Sleep(time.Second)
+	err := c.send("PING :" + pingSignature)
+	if err != nil {
+		t.Error(err)
+	}
+	select {
+	case <-rcvdPong:
+	case <-time.After(time.Second * 5):
+		t.Errorf("expected pong after 5 seconds")
+	}
+	err = c.send("PING")
+	if err != nil {
+		t.Error(err)
+	}
+	select {
+	case <-rcvdPong:
+	case <-time.After(time.Second * 5):
+		t.Errorf("expected pong after 5 seconds")
+	}
+
+	close(rcvdPong)
+	c.Disconnect()
 }
